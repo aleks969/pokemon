7b4b7363d6fb65332bf15f61aeb91ea77581e25d
aleks <aleks.mihaylov69@gmail.com>
2021-09-17 17:36:02 +0300

    fetch pokemons

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,19 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  const pokemonCount = 10;
+  const url =  `https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}`;
+    
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(pokemon => {
+        const li = document.createElement('li')
+        li.textContent = pokemon.name;
+        ul.appendChild(li);
+      })
+    });
+
+
+
 });
